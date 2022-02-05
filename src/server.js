8bbc90d65780a8952bdd552fdaa1fc3246a9d168
montanha1337 <matheusdteixeira@hotmail.com>
2022-02-05 18:52:18 -0300

    realização de acerto de ambiente

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ import app from './app';
 const PORT = process.env.PORT || 8080;
 //para ativar o backend coloque yarn dev
 app.listen(PORT);
-console.log(`Backend online !!!\nAberto na porta ${ PORT }`)
+console.log(`Backend online !!!`)
