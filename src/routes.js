e2509fa415e55b45b0d4c08a8ed18812ef58135b
montanha1337 <matheusdteixeira@hotmail.com>
2022-02-05 19:04:24 -0300

    acerto rotas

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Dev from './controller/Desenvolvedor'
 const routes = new Router();
 //olhar o final da rota 
 //web
-routes.use('/',(req,res)=>{ res.sendfile(__dirname+'/web/PaginaErro.html')})
+routes.use('/web',(req,res)=>{ res.sendfile(__dirname+'/web/PaginaErro.html')})
 //Api
 
 routes.use('/desenvolvimento',Dev) //Rota com ferramentas para auxiliar o frontend e demais areas
