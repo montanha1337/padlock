a7ac921a64eaf558fbee04a6b5889b67c9e9f791
montanha1337 <arthuratbee@gmail.com>
2022-10-16 14:05:25 -0300

    Correção de padronização

--- src/controller/pixController.js
+++ src/controller/pixController.js
@@ -1,10 +1,209 @@
-import Funcao from './functions'
+import Framework from './functions'
 import PixModel from '../model/Pix'
 import UserControl from './userController'
 import BancoControl from './BancoController'
 import validator from "validar-telefone"
 import ConfigControl from "./config"
 
+//#region VIEW
+async function Validador(token, container, rota) {
+    let validartoken = "sucesso"
+
+    //#region TRATAMENTO DE DADOS
+    let idUser = await Framework.ManipularToken("dev-retornaId", token)
+    if (idUser.status != 200)
+        return Framework.Framework.PadronizarRetorno("erro", idUser.status, idUser.message)
+
+    token = await Framework.ManipularToken("atualizar", token)
+    if (token.status != 200)
+        validartoken = "erro"
+    //#endregion
+
+    //#region CHAMADA DE METODOS
+    switch (rota) {
+        case "inserir":
+            await LogControl.deleta(email)
+            return await inserir(idUser.result, container.email, container.pix, container.Banco, container.tipo)
+
+        case "listarTodos":
+            return await listar(idUser.result, container.email)
+
+        case "alterar":
+            return await editar(idUser.result, container.email, container.pixAntigo, container.pixNovo, container.tipo, container.banco)
+
+        case "deleteId":
+            return await excluirId(idUser.result, container.email, container.pix)
+
+        case "listarUm":
+            return await listarUm(idUser.result, container.email, container.pix)
+
+        case "listarTipoPix":
+            return listarTipoPix()
+
+        default:
+            return Framework.PadronizarRetorno("erro", 400, "[Api] Erro ao informar a rota.")
+    }
+    //#endregion
+}
+//#endregion
+
+//#region METODOS
+async function inserir(user, emailUser, pix, banco, tipo) {
+    let result
+    let inserir = new Object()
+    let buscaUser
+    let buscaBanco
+    let valida = validaPix(pix, tipo)
+    if (valida.validador == false) {
+        return Framework.PadronizarRetorno("erro", 400, valida.mensagem)
+    }
+    pix = await Framework.encripta(pix)
+    user = await Framework.verificajwt(user)
+    if (user == false) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    }
+    valida = await PixModel.find({ user, emailUser, banco })
+    if (valida.length > 5) {
+        return Framework.PadronizarRetorno("erro", 400, "Quantidade de pix cadastrados excede o limite para este banco.")
+    }
+    result = await PixModel.create({ user, emailUser, tipo, pix, banco })
+    buscaUser = await Framework.atualizajwt(user)
+    buscaBanco = await BancoControl.listarUm(banco)
+    pix = await Framework.verificajwt(pix)
+    inserir = await organizaDados(pix, tipo, buscaBanco.result.fullNome)
+    inserir.token = buscaUser
+    return Framework.PadronizarRetorno("sucesso", 200, inserir)
+}
+
+async function listar(user, emailUser) {
+    let pix = new Object()
+    let listar = new Object()
+    let buscaPix
+    let buscaUser
+    let buscaBanco
+    let pixdescript
+    let userdescript = user
+
+    buscaPix = await PixModel.find({ userdescript, emailUser })
+    pix.dados = buscaPix
+    pix.tamanho = buscaPix.length
+    pix.lista = buscaPix
+    if (buscaPix[0]) {
+        buscaUser = await Framework.ManipularToken("criar", user)
+        if (token.status != 200)
+            return Framework.PadronizarRetorno("erro", 400, "Não foi possivel gerar autenticação.")
+
+        for (let i = 0; i < pix.tamanho; i++) {
+            buscaBanco = await BancoControl.listarUm(pix.dados[i].banco)
+            if (buscaBanco.status == 400) {
+                return buscaBanco
+            }
+            pixdescript = await Framework.ManipularDado("desencripta", pix.dados[i].pix)
+            pix.lista[i] = await organizaDados(pixdescript, pix.dados[i].tipo, buscaBanco.result.fullNome)
+            buscaBanco = ""
+        }
+        listar.token = buscaUser
+        listar.pix = pix.lista
+
+        return Framework.PadronizarRetorno("sucesso", 200, listar)
+    } else {
+        return Framework.PadronizarRetorno("erro", 400, "Não foi encontrado registros para este usuário.")
+    }
+}
+
+async function listarUm(user, emailUser, pixBusca) {
+    let pix = new Object()
+    let buscaPix
+    let buscaUser
+    let buscaBanco
+    user = await Framework.verificajwt(user)
+    if (user == false) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    }
+    buscaPix = await PixModel.find({ user, emailUser })
+    pix.tamanho = buscaPix.length
+    if (buscaPix[0]) {
+        for (let i = 0; i < pix.tamanho; i++) {
+            pix.encript = buscaPix[i].pix
+            buscaPix[i].pix = await Framework.verificajwt(buscaPix[i].pix)
+            if (buscaPix[i].pix == pixBusca) {
+                buscaUser = await UserControl.listarUm(user)
+                buscaBanco = await BancoControl.listarUm(buscaPix[i].banco)
+                pix.dados = await organizaDados(buscaUser.nome, buscaUser.email, pix.encript, buscaPix[i].tipo, buscaBanco.nome, buscaBanco.code, buscaBanco.fullNome)
+            } else {
+                pix.dados = Framework.PadronizarRetorno("erro", 400, "pix não encontrado.")
+            }
+        }
+        return Framework.PadronizarRetorno("sucesso", 200, pix.dados)
+    } else {
+        return Framework.PadronizarRetorno("erro", 400, "Email inválido.")
+    }
+}
+
+async function excluirId(user, email, pix) {
+    let pixBanco
+    let buscaPix
+    let tamanho
+    user = await Framework.verificajwt(user)
+    if (user == false) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    }
+    buscaPix = await PixModel.find({ user, email })
+    tamanho = buscaPix.length
+    for (let i = 0; i < tamanho; i++) {
+        pixBanco = await Framework.verificajwt(buscaPix[i].pix)
+        if (pix == pixBanco) {
+            await PixModel.findByIdAndDelete(buscaPix[i]._id)
+        }
+    }
+    buscaPix = await PixModel.find({ user, email })
+    tamanho = buscaPix.length
+    return Framework.PadronizarRetorno("sucesso", 200, tamanho)
+}
+
+async function editar(user, email, pixAntigo, pixNovo, tipo, banco) {
+    let valida = validaPix(pixNovo, tipo)
+    if (valida.validador == false) {
+        return Framework.PadronizarRetorno("erro", 400, valida.mensagem)
+    }
+    let pix = new Object()
+    let buscaPix
+    let userEncript = user
+    user = await Framework.verificajwt(user)
+    if (user == false) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    }
+    buscaPix = await PixModel.find({ user, email })
+    pix.tamanho = buscaPix.length
+    pix.novo = await Framework.encripta(pixNovo)
+    if (buscaPix[0]) {
+        for (let i = 0; i < pix.tamanho; i++) {
+            pix.encript = buscaPix[i].pix
+            buscaPix[i].pix = await Framework.verificajwt(buscaPix[i].pix)
+            if (buscaPix[i].pix == pixAntigo) {
+                await PixModel.findOneAndUpdate({ user, email, pix: pix.encript }, { pix: pix.novo, tipo, banco })
+                pix.dados = await listarUm(userEncript, email, pixNovo)
+                return Framework.PadronizarRetorno("sucesso", 200, pix.dados)
+            } else {
+                pix.dados = Framework.PadronizarRetorno("erro", 400, "pix não encontrado.")
+            }
+        }
+        return Framework.PadronizarRetorno("sucesso", 200, pix.dados)
+    } else {
+        return Framework.PadronizarRetorno("erro", 400, "Não existe pix para este usuario.")
+    }
+}
+
+async function listarTipoPix() {
+    let result = await ConfigControl.listarTipoPix()
+    if (result.length > 1)
+        return Framework.PadronizarRetorno("sucesso", 200, result)
+    else
+        return Framework.PadronizarRetorno("erro", 400, "Não foi possivel retornar os dados.")
+}
+//#endregion
+
+//#region AUXILIARES
 function validaPix(pix, tipo) {
     let valida = new Object()
     let aleatorio
@@ -12,13 +211,13 @@ function validaPix(pix, tipo) {
     valida.mensagem = "Pix Inválido."
     switch (tipo) {
         case "cpf":
-            valida.validador = Funcao.validaCpf(pix)
+            valida.validador = Framework.validaCpf(pix)
             return valida
         case "cnpj":
-            valida.validador = Funcao.validaCnpj(pix)
+            valida.validador = Framework.validaCnpj(pix)
             return valida
         case "email":
-            valida.validador = Funcao.validaEmail(pix)
+            valida.validador = Framework.validaEmail(pix)
             return valida
         case "telefone":
             valida.validador = validator(pix)
@@ -46,166 +245,6 @@ async function organizaDados(pix, tipo, nomeBanco) {
     organiza.banco = nomeBanco
     return organiza
 }
+//#endregion
 
-async function inserir(user, emailUser, pix, banco, tipo) {
-    let result
-    let inserir = new Object()
-    let buscaUser
-    let buscaBanco
-    let valida = validaPix(pix, tipo)
-    if (valida.validador == false) {
-        return Funcao.PadronizarRetorno("erro", 400, valida.mensagem)
-    }
-    pix = await Funcao.encripta(pix)
-    user = await Funcao.verificajwt(user)
-    if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
-    }
-    valida = await PixModel.find({ user, emailUser, banco })
-    if (valida.length > 5) {
-        return Funcao.PadronizarRetorno("erro", 400, "Quantidade de pix cadastrados excede o limite para este banco.")
-    }
-    result = await PixModel.create({ user, emailUser, tipo, pix, banco })
-    buscaUser = await Funcao.atualizajwt(user)
-    buscaBanco = await BancoControl.listarUm(banco)
-    pix = await Funcao.verificajwt(pix)
-    inserir = await organizaDados( pix, tipo, buscaBanco.result.fullNome)
-    inserir.token = buscaUser
-    return Funcao.PadronizarRetorno("sucesso",200,inserir)
-}
-
-async function listar(user, emailUser) {
-    let pix = new Object()
-    let listar = new Object()
-    let buscaPix
-    let buscaUser
-    let buscaBanco
-    let pixdescript
-    let userdescript = await Funcao.verificajwt(user)
-    if (userdescript == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
-    }
-    buscaPix = await PixModel.find({ userdescript, emailUser })
-    pix.dados = buscaPix
-    pix.tamanho = buscaPix.length
-    pix.lista = buscaPix
-    if (buscaPix[0]) {
-        buscaUser = await Funcao.atualizajwt(user)
-        for (let i = 0; i < pix.tamanho; i++) {
-            buscaBanco = await BancoControl.listarUm(pix.dados[i].banco)
-            if (buscaBanco.status == 400) {
-                return buscaBanco
-            }
-            pixdescript = await Funcao.verificajwt(pix.dados[i].pix)
-            pix.lista[i] = await organizaDados(pixdescript, pix.dados[i].tipo,buscaBanco.result.fullNome)
-            buscaBanco=""
-        }
-        listar.token = buscaUser
-        listar.pix = pix.lista
-        
-        return Funcao.PadronizarRetorno("sucesso",200,listar)
-    } else {
-        return Funcao.PadronizarRetorno("erro", 400, "Não foi encontrado registros para este usuário.")
-    }
-}
-
-async function listarUm(user, emailUser, pixBusca) {
-    let pix = new Object()
-    let buscaPix
-    let buscaUser
-    let buscaBanco
-    user = await Funcao.verificajwt(user)
-    if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
-    }
-    buscaPix = await PixModel.find({ user, emailUser })
-    pix.tamanho = buscaPix.length
-    if (buscaPix[0]) {
-        for (let i = 0; i < pix.tamanho; i++) {
-            pix.encript = buscaPix[i].pix
-            buscaPix[i].pix = await Funcao.verificajwt(buscaPix[i].pix)
-            if (buscaPix[i].pix == pixBusca) {
-                buscaUser = await UserControl.listarUm(user)
-                buscaBanco = await BancoControl.listarUm(buscaPix[i].banco)
-                pix.dados = await organizaDados(buscaUser.nome, buscaUser.email, pix.encript, buscaPix[i].tipo, buscaBanco.nome, buscaBanco.code, buscaBanco.fullNome)
-            } else {
-                pix.dados = Funcao.PadronizarRetorno("erro", 400, "pix não encontrado.")
-            }
-        }
-        return Funcao.PadronizarRetorno("sucesso",200,pix.dados)
-    } else {
-        return Funcao.PadronizarRetorno("erro", 400, "Email inválido.")
-    }
-}
-
-async function excluirId(user, email, pix) {
-    let pixBanco
-    let buscaPix
-    let tamanho
-    user = await Funcao.verificajwt(user)
-    if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
-    }
-    buscaPix = await PixModel.find({ user, email })
-    tamanho = buscaPix.length
-    for (let i = 0; i < tamanho; i++) {
-        pixBanco = await Funcao.verificajwt(buscaPix[i].pix)
-        if (pix == pixBanco) {
-            await PixModel.findByIdAndDelete(buscaPix[i]._id)
-        }
-    }
-    buscaPix = await PixModel.find({ user, email })
-    tamanho = buscaPix.length
-    return Funcao.PadronizarRetorno("sucesso",200,tamanho)
-}
-
-async function editar(user, email, pixAntigo, pixNovo, tipo, banco) {
-    let valida = validaPix(pixNovo, tipo)
-    if (valida.validador == false) {
-        return Funcao.PadronizarRetorno("erro", 400, valida.mensagem)
-    }
-    let pix = new Object()
-    let buscaPix
-    let userEncript = user
-    user = await Funcao.verificajwt(user)
-    if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
-    }
-    buscaPix = await PixModel.find({ user, email })
-    pix.tamanho = buscaPix.length
-    pix.novo = await Funcao.encripta(pixNovo)
-    if (buscaPix[0]) {
-        for (let i = 0; i < pix.tamanho; i++) {
-            pix.encript = buscaPix[i].pix
-            buscaPix[i].pix = await Funcao.verificajwt(buscaPix[i].pix)
-            if (buscaPix[i].pix == pixAntigo) {
-                await PixModel.findOneAndUpdate({ user, email, pix: pix.encript }, { pix: pix.novo, tipo, banco })
-                pix.dados = await listarUm(userEncript, email, pixNovo)
-                return Funcao.PadronizarRetorno("sucesso",200,pix.dados)
-            } else {
-                pix.dados = Funcao.PadronizarRetorno("erro", 400, "pix não encontrado.")
-            }
-        }
-        return Funcao.PadronizarRetorno("sucesso",200,pix.dados)
-    } else {
-        return Funcao.PadronizarRetorno("erro", 400, "Não existe pix para este usuario.")
-    }
-}
-
-function testePix(pix, tipo) {
-    pix = validaPix(pix, tipo)
-    if (pix.validador == true) {
-        return Funcao.PadronizarRetorno("sucesso",200,"pix Válido")
-    }
-    return Funcao.PadronizarRetorno("erro", 400, "Pix Inválido")
-}
-
-async function listarTipoPix() {
-    let result = await ConfigControl.listarTipoPix()
-    if (result.length> 1)
-        return Funcao.PadronizarRetorno("sucesso",200,result)
-    else
-        return Funcao.PadronizarRetorno("erro", 400, "Não foi possivel retornar os dados.")
-}
-
-module.exports = { inserir, listar, listarUm, excluirId, editar, testePix, listarTipoPix }
+module.exports = { inserir, listar, listarUm, excluirId, editar, listarTipoPix }
--- src/view/PixView.js
+++ src/view/PixView.js
@@ -3,58 +3,100 @@ import PixControl from '../controller/pixController'
 
 const router = express.Router()
 
+
+//#region CRUD
 router.post('/inserir', async (req, res) => {
-  const idUser = req.headers.authorization.replace(/^Bearer\s/, '');
-  const email = req.body.email
-  const pix = req.body.pix
-  const idBanco = req.body.idBanco
-  const tipo = req.body.tipo
-  let inserir = await PixControl.inserir(idUser, email, pix, idBanco, tipo)
-  res.status(inserir.status).json(inserir)
+  try {
+    const idUser = req.headers.authorization.replace(/^Bearer\s/, '');
+    const email = req.body.email
+    const pix = req.body.pix
+    const idBanco = req.body.idBanco
+    const tipo = req.body.tipo
+    let inserir = await PixControl.inserir(idUser, email, pix, idBanco, tipo)
+    res.status(inserir.status).json(inserir)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+
 router.post('/listar', async (req, res) => {
-  const token = req.headers.authorization.replace(/^Bearer\s/, '');
-  const email = req.body.email
-  let listar = await PixControl.listar(token, email)
-  res.status(listar.status).json(listar)
+  try {
+    const token = req.headers.authorization.replace(/^Bearer\s/, '');
+    const email = req.body.email
+    let listar = await PixControl.listar(token, email)
+    res.status(listar.status).json(listar)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+
 router.post('/listarum', async (req, res) => {
-  const token = req.headers.authorization.replace(/^Bearer\s/, '');
-  const email = req.body.email
-  const pix = req.body.pix
-  let listar = await PixControl.listarUm(token, email, pix)
-  res.status(listar.status).json(listar)
+  try {
+    const token = req.headers.authorization.replace(/^Bearer\s/, '');
+    const email = req.body.email
+    const pix = req.body.pix
+    let listar = await PixControl.listarUm(token, email, pix)
+    res.status(listar.status).json(listar)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+
 router.delete('/deletar', async (req, res) => {
-  const token = req.headers.authorization.replace(/^Bearer\s/, '');
-  const email = req.body.email
-  const pix = req.body.pix
-  let deletar = await PixControl.excluirId(token, email, pix)
-  res.status(deletar.status).json(deletar)
+  try {
+    const token = req.headers.authorization.replace(/^Bearer\s/, '');
+    const email = req.body.email
+    const pix = req.body.pix
+    let deletar = await PixControl.excluirId(token, email, pix)
+    res.status(deletar.status).json(deletar)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+
 router.put('/editar', async (req, res) => {
-  const token = req.headers.authorization.replace(/^Bearer\s/, '');
-  const email = req.body.email
-  const pixAntigo = req.body.pixAntigo
-  const pixNovo = req.body.pixNovo
-  const tipo = req.body.tipo
-  const banco = req.body.banco
-  let editar = await PixControl.editar(token, email, pixAntigo, pixNovo, tipo, banco)
-  res.status(editar.status).json(editar)
+  try {
+    const token = req.headers.authorization.replace(/^Bearer\s/, '');
+    const email = req.body.email
+    const pixAntigo = req.body.pixAntigo
+    const pixNovo = req.body.pixNovo
+    const tipo = req.body.tipo
+    const banco = req.body.banco
+    let editar = await PixControl.editar(token, email, pixAntigo, pixNovo, tipo, banco)
+    res.status(editar.status).json(editar)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+//#endregion
 
+//#region FERRAMENTAS
 router.post('/validapix', async (req, res) => {
-  const pix = req.body.pix
-  const tipo = req.body.tipo
-  let valida = await PixControl.testePix(pix, tipo)
-  res.status(valida.status).json(valida)
+  try {
+    const pix = req.body.pix
+    const tipo = req.body.tipo
+    let valida = await PixControl.testePix(pix, tipo)
+    res.status(valida.status).json(valida)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
+
 router.get('/listartipopix', async (req, res) => {
-  let tipo = await PixControl.listarTipoPix()
-  res.status(tipo.status).json(tipo)
+  try {
+    let tipo = await PixControl.listarTipoPix()
+    res.status(tipo.status).json(tipo)
+  } catch (e) {
+    let conexao = Framework.PadronizarRetorno("erro", 400, `Não foi possivel retornar requisição: ${e.message}`)
+    res.status(conexao.status).json(conexao)
+  }
 })
-
-
-
+//#endregion
 
 module.exports = router
