2500d79d07e0d3e19addb62abbb5dbf1a11603eb
montanha1337 <arthuratbee@gmail.com>
2022-10-23 22:33:16 -0300

    estabilixação contato

--- src/controller/contatoController.js
+++ src/controller/contatoController.js
@@ -1,11 +1,11 @@
-import Funcao from './functions'
+import Framework from './functions'
 import ContatoModel from '../model/contato'
 import PixControl from './pixController'
 
 async function formataDados(pix, tipo) {
-    pix = await Funcao.verificajwt(pix)
+    pix = await Framework.verificajwt(pix)
     if (pix == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Chave pix incorreta")
+        return Framework.PadronizarRetorno("erro", 400, "Chave pix incorreta")
     }
     let format = new Object()
     format.pix = pix
@@ -20,15 +20,15 @@ async function inserir(IdUser, nome, pixNovo, tipo) {
     if (valida.status == 400) {
         return valida
     }
-    pixNovo = await Funcao.encripta(pixNovo)
-    IdUser = await Funcao.verificajwt(IdUser)
+    pixNovo = await Framework.ManipularDado("encripta",pixNovo)
+    IdUser = await Framework.ManipularToken("dev-retornaId",IdUser)
     if (IdUser == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
     pix.pix = pixNovo
     pix.tipo = tipo
     inserir.adicionar = await ContatoModel.insertMany({ IdUser, nome, pix })
-    return Funcao.PadronizarRetorno("sucesso",200,inserir)
+    return Framework.PadronizarRetorno("sucesso",200,inserir)
 }
 
 async function adicionarPix(IdUser, nome, pixNovo, tipo) {
@@ -37,110 +37,109 @@ async function adicionarPix(IdUser, nome, pixNovo, tipo) {
     let valida = PixControl.testePix(pixNovo, tipo)
     inserir.result = true
     if (valida.status == 400) {
-        return Funcao.PadronizarRetorno("erro", 400, valida.mensagem)
+        return Framework.PadronizarRetorno("erro", 400, valida.mensagem)
     }
-    pixNovo = await Funcao.encripta(pixNovo)
-    IdUser = await Funcao.verificajwt(IdUser)
-    if (IdUser == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    pixNovo = await Framework.ManipularDado("encripta",pixNovo)
+    IdUser = await Framework.ManipularToken("dev-retornaId",IdUser)
+    if (IdUser.status != 200) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
     pix.pix = pixNovo
     pix.tipo = tipo
     inserir.adicionar = await ContatoModel.updateOne({ IdUser, nome }, { $push: { pix: pix } })
     if (inserir.adicionar.matchedCount !== 1) {
-        return Funcao.PadronizarRetorno("erro", 400, "Não possivel completar a adição.")
+        return Framework.PadronizarRetorno("erro", 400, "Não possivel completar a adição.")
     }
-    IdUser = await Funcao.gerajwt(IdUser)
-    return Funcao.PadronizarRetorno("sucesso",200,{token: IdUser})
+    IdUser = await Framework.ManipularToken("gera",IdUser)
+    return Framework.PadronizarRetorno("sucesso",200,{token: IdUser})
 }
 
 async function listar(IdUser,contato) {
     let listar = new Object()
     let id
     listar = []
-    IdUser = await Funcao.verificajwt(IdUser)
-    if (IdUser == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+    IdUser = await Framework.ManipularToken("dev-retornaId",IdUser)
+    if (IdUser.status != 200) {
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
-    let lista = await ContatoModel.find({ IdUser, nome: contato })
+    let lista = await ContatoModel.find({ IdUser: IdUser.result, nome: contato })
     if(lista == ""){
-        return Funcao.PadronizarRetorno("erro", 400, "Não foi encontrados contatos para este usuario.")
+        return Framework.PadronizarRetorno("erro", 400, "Não foi encontrados contatos para este usuario.")
     }
-    id= await Funcao.gerajwt(lista[0].id)
     for (let i = 0; i < lista[0].pix.length; i++) {
         listar[i] = await formataDados( lista[0].pix[i].pix, lista[0].pix[i].tipo)
     }
-    IdUser = await Funcao.gerajwt(IdUser)
-    return Funcao.PadronizarRetorno("sucesso",200,{token:IdUser, Contato: lista[0].nome, pix: listar })
+    IdUser = await Framework.ManipularToken("gera",IdUser)
+    return Framework.PadronizarRetorno("sucesso",200,{token:IdUser.result, Contato: lista[0].nome, pix: listar })
 }
 
 async function listarContato(IdUser) {
     let listar = new Object()
     listar = []
-    IdUser = await Funcao.verificajwt(IdUser)
+    IdUser = await Framework.verificajwt(IdUser)
     if (IdUser == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
     let lista = await ContatoModel.find({ IdUser })
     if(lista == ""){
-        return Funcao.PadronizarRetorno("erro", 400, "Não foi encontrados contatos para este usuario.")
+        return Framework.PadronizarRetorno("erro", 400, "Não foi encontrados contatos para este usuario.")
     }
     for (let i = 0; i < lista.length; i++) {        
         listar[i] = {Nome: lista[i].nome}
     }
-    IdUser = await Funcao.gerajwt(IdUser)
-    return Funcao.PadronizarRetorno("sucesso",200,{token:IdUser, Contato: listar})
+    IdUser = await Framework.gerajwt(IdUser)
+    return Framework.PadronizarRetorno("sucesso",200,{token:IdUser, Contato: listar})
 }
 
 async function EditarContato(user,contato,nome){
-    user = await Funcao.verificajwt(user)
+    user = await Framework.verificajwt(user)
     if(user==false){
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não Encontrado")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não Encontrado")
     }
-    contato = await Funcao.verificajwt(contato)
+    contato = await Framework.verificajwt(contato)
     if(contato==false){
-        return Funcao.PadronizarRetorno("erro", 400, "Contato não Encontrado")
+        return Framework.PadronizarRetorno("erro", 400, "Contato não Encontrado")
     }
     await ContatoModel.findOneAndUpdate({IdUser:user,_id:contato},{nome})
-    return Funcao.PadronizarRetorno("sucesso",200,"")
+    return Framework.PadronizarRetorno("sucesso",200,"")
 }
 
 async function listarUm(user,contato) {
     let listar
-    user = await Funcao.verificajwt(user)
+    user = await Framework.verificajwt(user)
     if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
-    contato = await Funcao.verificajwt(contato)
+    contato = await Framework.verificajwt(contato)
     if (contato == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Contato não identificado!!!")
+        return Framework.PadronizarRetorno("erro", 400, "Contato não identificado!!!")
     }
     listar = await ContatoModel.findById({IdUser:user,_id:contato},nome)
-    return Funcao.PadronizarRetorno("sucesso",200,listar)
+    return Framework.PadronizarRetorno("sucesso",200,listar)
 }
 
 async function excluirContato(idUser,Contato) {
-    idUser = await Funcao.verificajwt(idUser)
+    idUser = await Framework.verificajwt(idUser)
 
     if(idUser==false)
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não Encontrado")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não Encontrado")
 
     await ContatoModel.deleteMany({IdUser:idUser,nome:Contato})
     let busca = await ContatoModel.find({IdUser:idUser,nome:Contato})
 
     if(busca=="")
-        return Funcao.PadronizarRetorno("sucesso",200,{message:"Deletado com sucesso"})
+        return Framework.PadronizarRetorno("sucesso",200,{message:"Deletado com sucesso"})
     else 
-        return Funcao.PadronizarRetorno("erro", 400, {message:"Erro ao deletar"})
+        return Framework.PadronizarRetorno("erro", 400, {message:"Erro ao deletar"})
 }
 
 async function excluirPix(user,pix) {
-    user = await Funcao.verificajwt(user)
+    user = await Framework.verificajwt(user)
     if (user == false) {
-        return Funcao.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
+        return Framework.PadronizarRetorno("erro", 400, "Usuario não identificado!!!")
     }
     let result = await ContatoModel.findOneAndDelete({IdUser:user,pix:{pix}})
-    return Funcao.PadronizarRetorno("sucesso",200,result)
+    return Framework.PadronizarRetorno("sucesso",200,result)
 }
 
 module.exports = { inserir, adicionarPix, EditarContato, listar, listarContato, listarUm, excluirContato, excluirPix }
